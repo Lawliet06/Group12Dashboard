96d43ca7082985cc5e78b4dc645848c6470e8a38
Mike-Charles <mikecharlemin.com>
2024-09-13 11:25:26 +0200

    MAde some chANGES

--- States.js
+++ States.js
@@ -1,20 +1,20 @@
-fetch("http://localhost:3000/dash_stats")
+// fetch("http://localhost:3000/dash_stats")
 
-    .then((response) => {
-        if(response.ok){
+//     .then((response) => {
+//         if(response.ok){
 
-            console.log("fetched");
-            return response.json();
+//             console.log("fetched");
+//             return response.json();
 
-        }else{
-            throw new Error("failed")
-        }
-    })
-    .then((data) => {
-    document.getElementById('users').textContent = data.users;
-    document.getElementById('products').textContent = data.products;
-    document.getElementById('sales').textContent = data.sales;
-    document.getElementById('profit').textContent = data.profit;
-    })
+//         }else{
+//             throw new Error("failed")
+//         }
+//     })
+//     .then((data) => {
+//     document.getElementById('users').textContent = data.users;
+//     document.getElementById('products').textContent = data.products;
+//     document.getElementById('sales').textContent = data.sales;
+//     document.getElementById('profit').textContent = data.profit;
+//     })
     
-    .catch(error => console.error(error));
+//     .catch(error => console.error(error));
